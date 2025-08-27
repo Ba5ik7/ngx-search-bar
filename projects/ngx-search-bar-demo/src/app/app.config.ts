83c41e999f502c54a5a3acdbbae649a47ccac8e8
Wesley DuSell <dusellfam@gmail.com>
2025-08-27 01:04:05 -0400

    Zoneless

--- projects/ngx-search-bar-demo/src/app/app.config.ts
+++ projects/ngx-search-bar-demo/src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { provideHttpClient } from '@angular/common/http';
 import {
   ApplicationConfig,
-  // provideExperimentalZonelessChangeDetection,
+  provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
-    provideHttpClient()
-    // provideExperimentalZonelessChangeDetection(),
+    provideHttpClient(),
+    provideZonelessChangeDetection(),
   ],
 };
